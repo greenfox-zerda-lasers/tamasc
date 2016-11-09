006ce4b34468dd479ddd5bd8b95348d8c0ed56d5
tamasc <tamas.czerjak@gmail.com>
2016-11-09 22:54:46 +0100

    draw triangles

--- week-04/day-3/triangles.py
+++ week-04/day-3/triangles.py
@@ -11,15 +11,15 @@ def line(x0, y0, x1, y1):
 canvas = Canvas(width=canvas_size*1.3, height=canvas_size)
 canvas.pack()
 
-def triangles(num, side):
+def triangles(num, side, x0, y0): #num=max number of triangles in a row, side=the length of the main triangle(length), x0,y0=top corner position of the main traingle
     x = int(side/num)
-    print(x)
-    for i in range(0, side, x):
-        line(side/2-i*1/2, 0+i*math.sqrt(3)/2, side/2+i*1/2, 0+i*math.sqrt(3)/2)
-        line(0, side/2, side/2+i/2, 0+i*math.sqrt(3)/2)
+    for i in range(0, side+2, x):
+        line(x0-side/2+side/2-i*1/2, y0+0+i*math.sqrt(3)/2, x0-side/2+side/2+i*1/2, y0+0+i*math.sqrt(3)/2)
+        line(x0-side/2+0+i, y0+side*math.sqrt(3)/2, x0-side/2+side/2+i/2, y0+0+i*math.sqrt(3)/2)
+        line(x0-side/2+side/2-i/2, y0+0+i*math.sqrt(3)/2, x0-side/2+side-i, y0+side*math.sqrt(3)/2)
 
 
-triangles(10, 300)
+triangles(120, 500,250,00)
 
 
 master.mainloop()
