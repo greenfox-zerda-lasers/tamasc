46aae65c5e52ed03fc2adc2be0c2e60dbbd584cb
tamasc <tamas.czerjak@gmail.com>
2016-11-10 20:57:59 +0100

    small changes in hexagon

--- week-04/day-4/13.py
+++ week-04/day-4/13.py
@@ -7,18 +7,20 @@ canvas.pack()
 
 def draw(n, size=400, x=0, y=0):
     colors = ['black', 'yellow', 'purple', 'blue', 'red']
-    color1 = colors[n%len(colors)]
-    color2 = colors[(n+3)%len(colors)]
-    if n == 0:
+    color1 = colors[n%len(colors)-1]
+    color2 = colors[(n-1)%len(colors)-2]
+    if n == 0 or size < 3:
         return
     else:
-        canvas.create_polygon(20+x+size/4, 20+y+0, 20+x+size*3/4, 20+y+0,
-                                20+x+size, 20+y+size/2, 20+x+size*3/4, 20+y+size,
-                                20+x+size*1/4, 20+y+size, 20+x+0, 20+y+size/2, fill=color1, outline=color2)
+        canvas.create_polygon(x+size/4, y+0, x+size*3/4, y+0,
+                                x+size, y+size/2, x+size*3/4, y+size,
+                                x+size*1/4, y+size, x+0, y+size/2,
+                                fill=color1, outline=color2)
         n = n - 1
+        print(size/2, x+size/8)
         draw(n, size/2, x+size/8, y+0)
-        draw(n, size/2, x+size*4/8, y+size/4)
-        draw(n, size/2, x+size/8, y+size/2)
+        draw(n, size/2, x+size*4/8, y+size*2/8)
+        draw(n, size*4/8, x+size/8, y+size*4/8)
 
-draw(8)
+draw(4, 600)
 master.mainloop()
