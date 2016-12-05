ef018df6aaa32c2f46f4e4c930a47b467ecb1aa2
tamasc <tamas.czerjak@gmail.com>
2016-12-05 15:52:49 +0100

    add tasks and finish some of them

---
+++ week-07/05.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var e = 8;
+// cube e's value
+
+e *= e * e;
+
+console.log(e);
---
+++ week-07/13.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var m = 'Apple'
+// fill the m variable with its content 4 times
+
+m = m.repeat(4);
+
+console.log(m);
