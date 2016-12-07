2fdbca580aedf656e60ed9b56abea5aafe46dabb
tamasc <tamas.czerjak@gmail.com>
2016-12-07 10:59:40 +0100

    add tasks for week-7

---
+++ week-07/day-3/01.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Create a function that takes a number and returns it as string in english
+// like 0 -> "zero", it should work with the first 5 numbers, after it should
+// return "many"
+
+function numbersLiteral(number) {
+  switch (number) {
+    case 0:
+      console.log('zero');
+      break;
+    case 1:
+      console.log('one');
+      break;
+    case 2:
+      console.log('two');
+      break;
+    default:
+      console.log('many');
+      break;
+  }
+}
+
+numbersLiteral(1);
