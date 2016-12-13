5d8fe3908a4309fd4e895c11390ca6a83c94f7e1
tamasc <tamas.czerjak@gmail.com>
2016-12-13 16:18:38 +0100

    task 8

--- week-08/day-2/08.js
+++ week-08/day-2/08.js
@@ -1,5 +1,27 @@
-// Create a "tour" function that takes two functions as parameters: 
+// Create a "tour" function that takes two functions as parameters:
 //  - walk and distance
-//  - distance should return an array of false values [false,false,false] with the length of a given parameter
+//  - distance should return an array of false values [false,false,false]
+// with the length of a given parameter
 //  - walk should go through the returned array of distance and change it to true
 //  - tour should return the result of walk
+
+function tour(walk, distance) {
+  var length = 5;
+  return walk(distance(length));
+}
+
+function distance(length) {
+  var arr = [];
+  for (var i = 0; i < length; i++) {
+    arr.push(false);
+  }
+  return arr;
+}
+
+function walk(arr) {
+  return arr.map(function(e) {
+    return !e;
+  });
+}
+
+console.log(tour(walk, distance));
