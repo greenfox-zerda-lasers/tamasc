62ec384159c9d3017aadaa3978895ee111887c85
tamasc <tamas.czerjak@gmail.com>
2016-12-15 23:02:33 +0100

    modify main.js

--- Todo-JS/main.js
+++ Todo-JS/main.js
@@ -1,22 +1,51 @@
 'use strict';
 
+// ***************Functionality******************
+
+document.querySelector('input[type=button]').addEventListener('click', function(){
+   ajax.getTodos();
+});
+
+var ul = document.querySelector('ul');
+
+// ****************Ajax object*******************
 
 var Ajax = function() {};
 
 Ajax.prototype.getTodos = function() {
+  var data;
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos', true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send();
   xhr.onreadystatechange = function(rsp) {
     if (xhr.readyState === XMLHttpRequest.DONE){
-      console.log(JSON.parse(xhr.response));
+      data = JSON.parse(xhr.response).reverse();
+      app.refresh(data);
     }
   };
 };
 
 var ajax = new Ajax();
 
-document.querySelector('input[type=button]').addEventListener('click', function(){
-  ajax.getTodos();
-});
+
+// ****************App object*******************
+
+var App = function() {};
+
+App.prototype.refresh = function(data) {
+   document.querySelectorAll('li').forEach(function(item) {
+      ul.removeChild(item);
+   });
+   data.forEach(function (e) {
+      var item = document.createElement('li');
+      item.innerHTML = e.text;
+      if (!e.completed) {
+         item.className = 'completed';
+      }
+      ul.appendChild(item);
+   });
+};
+
+var app = new App();
+app.refresh(ajax.getTodos());
