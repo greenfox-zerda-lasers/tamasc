a3fd08193a6d6503a8eced6a9da729153db4ba5e
tamasc <tamas.czerjak@gmail.com>
2016-12-19 16:58:03 +0100

    add post request

--- week-09/day-1/test-server/task2.js
+++ week-09/day-1/test-server/task2.js
@@ -1,26 +1,13 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var data = require('./data.json');
 
 var app = express();
+app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var data = [
-  {
-      "completed": false,
-      "id": 1,
-      "text": "Buy milk"
-  },
-  {
-      "completed": false,
-      "id": 2,
-      "text": "Make dinner"
-  },
-  {
-      "completed": false,
-      "id": 3,
-      "text": "Save the world"
-  }
-];
 
 app.get('/todos', function a(req, res) {
   res.set('Content-Type', 'application/json');
@@ -30,15 +17,26 @@ app.get('/todos', function a(req, res) {
 app.get('/:id', function a(req, res) {
   res.set('Content-Type', 'application/json');
   res.send(
-    JSON.stringify(data.filter(function(e) {
+    data.filter(function(e) {
       return parseInt(req.params.id) === e["id"];
-    }))
+    })
   );
 });
 
 app.get('/*', function a(req, res) {
   console.log(req.url);
   res.send(String(new Date()) + ' \nhajajajajajajjjjj\n' + req.url);
+});
+
+app.post('/todos', urlencodedParser, function a(req, res) {
+  console.log(req.body);
+  var todo = {
+    completed: Boolean(req.body.completed),
+    id: data.length+1,
+    text: req.body.text,
+  };
+  data.push(todo);
+  res.send(JSON.stringify(todo));
 });
 
 app.listen(3000);
