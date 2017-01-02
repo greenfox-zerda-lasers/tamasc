68d0fc651358618b2044d21f05d9b9f392930e55
tamasc <tamas.czerjak@gmail.com>
2017-01-02 23:52:21 +0100

    mocked backend? with fake database

--- Foxplayer/backend/server.js
+++ Foxplayer/backend/server.js
@@ -8,26 +8,33 @@ var data = require('./data.json');
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
 
 var fs = require('fs');
 var mm = require('musicmetadata');
 
 
 app.get('/', function a(req, res) {
-  var songList = [];
-  for (var i = 0, l = data.songs.length; i < l; i++){
-    console.log(data.songs[i].url);
-    var stream = fs.createReadStream(data.songs[i].url);
-    var parser = mm(stream, function (err, metadata) {
-      if (err) throw err;
-      songList.push(metadata);
-      if (data.songs.length == songList.length) {
-        res.send(songList);
-      }
-      stream.close();
-    });
-  }
+    var songList = [];
+    for (var i = 0, l = data.songs.length; i < l; i++) {
+        (function(e) { // was a reference problem!!!!!!!
+            var stream = fs.createReadStream(data.songs[e].url);
+            var parser = mm(stream, {
+                duration: true
+            }, function(err, metadata) {
+                if (err) throw err;
+                metadata.id = data.songs[e].id;
+                metadata.src = data.songs[e].url;
+                songList.push(metadata);
+                if (data.songs.length == songList.length) {
+                    res.send(songList);
+                }
+                stream.close();
+            });
+        })(i)
+    };
 });
 
 
--- Foxplayer/frontend/app.js
+++ Foxplayer/frontend/app.js
@@ -1,23 +1,65 @@
 'use strict';
 
-var ajax = (function() {
-  var list = function(callback) {
-    var data;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:3000/', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(dataToSend);
-    xhr.onreadystatechange = function(rsp) {
-      if (xhr.readyState === XMLHttpRequest.DONE){
-        data = JSON.parse(xhr.response);
-        callback(data);
-      }
-    };
+
+var app = (function() {
+
+  var logg = function (data) {
+     console.log(data);
   };
+
+  var renderSongList = function (songs) {
+     var row;
+     songs.forEach(function(song) {
+        var table = document.querySelector('tbody');
+        console.log(table);
+        row = document.createElement('tr');
+        row.src = song.src;
+        row.addEventListener('click', function() {
+         console.log();
+         document.querySelector('audio').src = this.src;
+        });
+        row = renderHelper('td', [song.id, song.artist[0] +" - "+song.title, song.duration], row);
+        table.appendChild(row);
+     });
+  };
+
+  var renderHelper = function(child, text, parent) {  //creates a HTML element and appends it to the given parent
+     text.forEach(function(t) {
+       var childElement = document.createElement(child);
+       childElement.innerText = t;
+       parent.appendChild(childElement);
+     });
+     return parent;
+  };
+
+  return {
+     renderSongList: renderSongList
+  };
+
 })();
 
-var render = (function() {
-  var list = function () {
-    
-  }
+
+var ajax = (function() {
+
+   var getSongList = function() {
+      var data;
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'http://localhost:3000/', true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send('');
+      xhr.onreadystatechange = function(rsp) {
+         if (xhr.readyState === XMLHttpRequest.DONE){
+            data = JSON.parse(xhr.response);
+            console.log('kaksi');
+            app.renderSongList(data);
+         }
+      };
+   };
+
+   return {
+      getSongList: getSongList
+   };
+
 })();
+
+ajax.getSongList();
