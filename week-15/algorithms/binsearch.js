78b3e9d2138e80d55f1663d36537322bbd09b8e6
tamasc <tamas.czerjak@gmail.com>
2017-02-07 17:32:01 +0100

    delete not working function from the file

--- week-15/algorithms/binsearch.js
+++ week-15/algorithms/binsearch.js
@@ -23,35 +23,8 @@ const binarySearch = function (array, number) {
   return false;
 }
 
-const binarySearch2 = function (array, number) { //eliminating all costly functions
-  let N = array.length - 1;
-  if (N % 2 === 0) {
-    array[N] = '';
-    N++;
-  }
-  let lowerBound = 0;
-  let upperBound = N;
-  if (number === array[lowerBound] || number === array[upperBound]) {
-    return true;
-  }
-  while (lowerBound !== upperBound ) {
-    let M = (lowerBound + upperBound)/2;
-    console.log(lowerBound, upperBound, M);
-    if (array[M] == number) {
-      return true;
-    } else {
-      if (array[M] > number) {
-        upperBound = M;
-      } else {
-        lowerBound = M;
-      }
-    }
-  }
-  return false;
-}
-
 let arr = require('./array.js');
-const x = 0;
+const x = 1;
 
 const start = new Date();
 console.log(binarySearch(arr, x));
