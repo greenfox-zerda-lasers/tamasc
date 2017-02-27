7c180728ae8ee7ffb73217251786145d5576f5be
tamasc <tamas.czerjak@gmail.com>
2017-02-27 22:16:07 +0100

    add 2 test, fails at the last

--- selection_week/emarsys/test.js
+++ selection_week/emarsys/test.js
@@ -13,7 +13,7 @@ test('returns an array of destenations with 4 elements', function (t) {
   t.end();
 });
 
-test('with full of dependent destenations return in a correct way', function (t) {
+test('with full of dependent destenations returns the correct order', function (t) {
   const horvat = new Nyaralas('4 => 3', '3 => 2', '2 => 1');
 
   var actual = horvat.showPath();
@@ -27,6 +27,25 @@ test('with circularly dependent destenations throws an error', function (t) {
   const horvat = new Nyaralas('4 => 3', '3 => 2', '2 => 1', '1 => 4');
 
   t.throws(horvat.showPath);
+  t.end();
+});
 
+test('with full of dependent and with an independent destenation at the begining returns the correct order', function (t) {
+  const horvat = new Nyaralas('h', '4 => 3', '3 => 2', '2 => 1');
+
+  var actual = horvat.showPath();
+  var expected = ['1', '2', '3', '4', 'h'];
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('with full of dependent and with an independent destenation at the end returns the correct order', function (t) {
+  const horvat = new Nyaralas('4 => 3', '3 => 2', '2 => 1', 'h');
+
+  var actual = horvat.showPath();
+  var expected = ['1', '2', '3', '4', 'h'];
+
+  t.deepEqual(actual, expected);
   t.end();
 });
